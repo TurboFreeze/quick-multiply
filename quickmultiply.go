ab9625b5a2b3ee3a727438f6002d3795033efddf
TurboFreeze <iturbofreeze@gmail.com>
2018-07-16 16:48:46 -0400

    collecting performance statistics

--- quickmultiply.go
+++ quickmultiply.go
@@ -24,6 +24,8 @@ func main() {
 	fmt.Println("* WELCOME TO QUICK MULTIPLY *")
 	fmt.Println("*****************************")
 
+	data_record := ""
+
 	// Continuous loop
 	for true {
 		// Numbers generated
@@ -41,7 +43,12 @@ func main() {
 		ans, _ := reader.ReadString('\n')
 		ans_int, err := strconv.Atoi(strings.TrimSpace(ans))
 		if err != nil {
-			fmt.Printf("%sSKIPPING%s due to invalid input, must be integer.\n", ANSI_YELLOW, ANSI_END)
+			// End timer
+			end_time := time.Now()
+			elapsed := end_time.Sub(start_time)
+
+			fmt.Printf("%sSKIPPING%s due to invalid input, must be integer. It took %.2f seconds.\n", ANSI_YELLOW, ANSI_END, elapsed.Seconds())
+			data_record += "0," + strconv.Itoa(num_first) + "," + strconv.Itoa(num_second) + "," + strconv.Itoa(prod) + ",," + strconv.FormatFloat(elapsed.Seconds(), 'f', 3, 64) // 0 = SKIP
 		} else {
 			// End timer
 			end_time := time.Now()
@@ -50,10 +57,15 @@ func main() {
 			// Check answer
 			if prod == ans_int {
 				fmt.Printf("%sCORRECT%s. It took %.2f seconds.\n", ANSI_GREEN, ANSI_END, elapsed.Seconds())
+				data_record += "2," + strconv.Itoa(num_first) + "," + strconv.Itoa(num_second) + "," + strconv.Itoa(prod) + "," + strconv.Itoa(ans_int) + "," + strconv.FormatFloat(elapsed.Seconds(), 'f', 3, 64) // 2 = CORRECT
 			} else {
-				fmt.Printf("%sINCORRECT%s. The actual answer is %d.\n", ANSI_RED, ANSI_END, prod)
+				fmt.Printf("%sINCORRECT%s. It took %.2f seconds. The actual answer is %d.\n", ANSI_RED, ANSI_END, elapsed.Seconds(), prod)
+				data_record += "1," + strconv.Itoa(num_first) + "," + strconv.Itoa(num_second) + "," + strconv.Itoa(prod) + "," + strconv.Itoa(ans_int) + "," + strconv.FormatFloat(elapsed.Seconds(), 'f', 3, 64) // -1 = INCORRECT
 			}
 		}
+		fmt.Println(data_record)
+		// Write the data
+		// Schema is CSV: Status, first, second, product (real answer), input (user answer), time
 
 		fmt.Println("*****************************")
 	}
